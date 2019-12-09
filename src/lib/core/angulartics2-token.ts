cc1fcf256f3e0c90e17e02b101d2f2ef310e62ac
Shahmir Noorani <shahmir.noorani@hundredxinc.com>
2019-12-09 17:00:34 -0600

    Creating dist branch

--- src/lib/core/angulartics2-token.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-import { Angulartics2Settings } from './angulartics2-config';
-
-export interface Angulartics2Token {
-  settings: Partial<Angulartics2Settings>;
-}
-
-export const ANGULARTICS2_TOKEN = new InjectionToken<Angulartics2Token>('ANGULARTICS2');
